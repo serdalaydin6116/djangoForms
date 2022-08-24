44dffc3053375fd910b638592b4e9160d5bd4de7
serdalaydin6116 <serdalaydin.6116@gmail.com>
2022-08-24 23:09:33 +0300

    crispy forms was added into settings.py

--- djform/settings.py
+++ djform/settings.py
@@ -38,8 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'firstForm',
+    'firstForm','crispy_forms',
 ]
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',

4a9a15fed0d922e5b1007d0be5ddfad75b23b440
serdalaydin6116 <serdalaydin.6116@gmail.com>
2022-08-24 22:12:55 +0300

    csrf plus form ınputs

--- firstForm/views.py
+++ firstForm/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
+from .forms import StudentForm
 
 # Create your views here.
 def index(request):
     return render(request, 'firstForm/index.html')
 
+# def student_page(request):
+#     return render(request,'firstForm/firstForm.html')
 def student_page(request):
-    return render(request,'firstForm/firstForm.html')
+    print(request.POST)
+    print(request.FILES)
+    form = StudentForm()
+    context ={
+        'form': form 
+    }
+    return render(request,'firstForm/firstForm.html', context) 

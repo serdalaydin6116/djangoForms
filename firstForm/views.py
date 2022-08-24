aa57127536ecb2fadc3ae210fa0016b75f6537c6
serdalaydin6116 <serdalaydin.6116@gmail.com>
2022-08-24 21:42:14 +0300

    firstForm was created

--- firstForm/views.py
+++ firstForm/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    return render(request, 'firstForm/index.html')
+
+def student_page(request):
+    return render(request,'firstForm/firstForm.html')

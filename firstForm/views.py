ed5a706db231aa2642fc274e2a5f205db08ffbf8
serdalaydin6116 <serdalaydin.6116@gmail.com>
2022-08-24 22:28:44 +0300

    form validation

--- firstForm/views.py
+++ firstForm/views.py
@@ -7,11 +7,31 @@ def index(request):
 
 # def student_page(request):
 #     return render(request,'firstForm/firstForm.html')
+# def student_page(request):
+#     print(request.POST)
+#     print(request.FILES)
+#     form = StudentForm()
+#     context ={
+#         'form': form 
+#     }
+#     return render(request,'firstForm/firstForm.html', context) 
+
 def student_page(request):
-    print(request.POST)
-    print(request.FILES)
     form = StudentForm()
-    context ={
-        'form': form 
+    if request.method == 'POST':
+        form = StudentForm(request.POST, request.FILES)
+        if form.is_valid():
+            student_data = {
+                "first_name": form.cleaned_data.get('first_name'),
+                "last_name": form.cleaned_data.get('last_name'),
+                "number": form.cleaned_data.get('number'),
+                "profile_pic": form.cleaned_data.get('profile_image'),
+            }
+            student = Student(**student_data)  #Student.objects.create(**student_data)
+            student.save()
+            return redirect('student')
+
+    context = {
+        'form': form
     }
-    return render(request,'firstForm/firstForm.html', context) 
+    return render(request, 'student/student.html', context)

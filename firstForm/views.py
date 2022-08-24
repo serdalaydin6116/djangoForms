af7a2fc06d7f360dcc94c32b6198714ac76f29f4
serdalaydin6116 <serdalaydin.6116@gmail.com>
2022-08-24 22:50:10 +0300

    views.py shortened version

--- firstForm/views.py
+++ firstForm/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import StudentForm
+from .models import Student
 
 # Create your views here.
 def index(request):
@@ -16,22 +17,35 @@ def index(request):
 #     }
 #     return render(request,'firstForm/firstForm.html', context) 
 
+# def student_page(request):
+#     form = StudentForm()
+#     if request.method == 'POST':
+#         form = StudentForm(request.POST, request.FILES)
+#         if form.is_valid():
+#             student_data = {
+#                 "first_name": form.cleaned_data.get('first_name'),
+#                 "last_name": form.cleaned_data.get('last_name'),
+#                 "number": form.cleaned_data.get('number'),
+#                 "profile_pic": form.cleaned_data.get('profile_image'),
+#             }
+#             student = Student(**student_data)  #Student.objects.create(**student_data)
+#             student.save()
+#             return redirect('index')
+
+#     context = {
+#         'form': form
+#     }
+#     return render(request, 'firstForm/firstForm.html', context)
+
 def student_page(request):
     form = StudentForm()
     if request.method == 'POST':
         form = StudentForm(request.POST, request.FILES)
         if form.is_valid():
-            student_data = {
-                "first_name": form.cleaned_data.get('first_name'),
-                "last_name": form.cleaned_data.get('last_name'),
-                "number": form.cleaned_data.get('number'),
-                "profile_pic": form.cleaned_data.get('profile_image'),
-            }
-            student = Student(**student_data)  #Student.objects.create(**student_data)
-            student.save()
-            return redirect('student')
+            form.save()
+            return redirect('index')
 
     context = {
         'form': form
     }
-    return render(request, 'student/student.html', context)
+    return render(request, 'firstForm/firstForm.html', context)
